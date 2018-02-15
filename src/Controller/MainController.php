45d3bb5db01d7561d62568b7dcbf1d3f4ed53289
Bidule <lekurs@gmail.com>
2018-02-15 08:31:22 +0100

    dev(reviews): make ReviewsType + entity

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,21 +10,45 @@ namespace App\Controller;
 
 
 use App\Form\ContactForm;
+use App\Helper\ContactMailHelper;
 use App\Helper\InstagramHelper;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
 class MainController extends Controller
 {
-    public function index()
+    public function index(Request $request, ContactMailHelper $contactMailHelper)
     {
       $title = "Bienvenue sur le site de Claire GINDRE - Photographe";
 
       $insta = new InstagramHelper();
       $json = $insta->show();
 
-      $contact = $this->createForm(ContactForm::class, array(
-          'action' => $this->generateUrl('contactPost'),
-          'method' => 'POST',
-      ));
+
+      $contactForm = new ContactForm();
+      $contact = $this->createForm(ContactForm::class);
+
+      $contact->handleRequest($request);
+
+      if($contact->isSubmitted() && $contact->isValid())
+      {
+          $contactForm = $contact->getData();
+
+          //Send Mail Confirm
+          $contactMailHelper->send($request);
+
+          $this->addFlash(
+              'contact_sucess',
+              'Votre mail a bien été envoyé'
+          );
+
+          $this->addFlash(
+              'contact_error',
+              'Erreur lors de l\'envoi du mail'
+          );
+
+          return $this->redirectToRoute('/');
+      }
 
       return $this->render('front/index.html.twig', array(
         'title' => $title,
