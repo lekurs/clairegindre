ec19a4111ada18f826ca697996d1711b62aeceec
Bidule <lekurs@gmail.com>
2018-05-11 13:22:12 +0200

    dev(refactoring - gallery): add creationDate attributes & field

---
+++ src/Services/Interfaces/PictureUploaderHelperInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Maxime GINDRE
+ * Date: 11/05/2018
+ * Time: 13:07
+ */
+
+namespace App\Services\Interfaces;
+
+
+interface PictureUploaderHelperInterface
+{
+
+}
