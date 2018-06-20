bc2cbe6485ab3e37ba861d637b41e9f89a3acd43
Bidule <lekurs@gmail.com>
2018-06-20 16:46:28 +0200

    dev(registration): update registration

--- src/UI/Form/FormHandler/RegistrationTypeHandler.php
+++ src/UI/Form/FormHandler/RegistrationTypeHandler.php
@@ -119,7 +119,7 @@ final class RegistrationTypeHandler implements RegistrationTypeHandlerInterface
                                                                             $form->getData()->dateWedding,
                                                                             $picture,
                                                                             '1',
-                                                                            'ROLE_USER',
+                                                                            'ROLE_ADMIN',
                                                                             $this->stringReplaceHelper->replace($form->getData()->username . '-' . $form->getData()->lastName)
                                                                         );
 
