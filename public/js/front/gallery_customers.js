6b4a1e6b360c779c87da01faaea6dcef8f2bfb0e
Bidule <lekurs@gmail.com>
2018-06-24 15:15:46 +0200

    test dl google

--- public/js/front/gallery_customers.js
+++ public/js/front/gallery_customers.js
@@ -16,8 +16,7 @@ $(document).ready(function () {
         $('.modal-perso').show('1500');
 
         $('.img-container').attr('src', imgId);
-        $('.link-to-dl-image').attr('href', imgId);
-        $('.link-to-dl-image').attr('download', imgUrl);
+        $('.link-to-dl-image').attr('href', imgUrl);
 
     });
 
