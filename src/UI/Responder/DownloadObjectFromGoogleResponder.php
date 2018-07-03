17158d7916acd2bed147f06c00bbf1f9cab0f6c8
Bidule <lekurs@gmail.com>
2018-07-04 00:08:54 +0200

    dev(dowload): fix jpg vs jpeg

--- src/UI/Responder/DownloadObjectFromGoogleResponder.php
+++ src/UI/Responder/DownloadObjectFromGoogleResponder.php
@@ -20,9 +20,6 @@ class DownloadObjectFromGoogleResponder implements DownloadObjectFromGoogleRespo
     {
         $file = $object;
 
-        dump($file);
-        die;
-
         header('Content-Type: application/octet-stream');
         header('Content-disposition: attachment; filename='.basename($file));
         header('Pragma: no-cache');
